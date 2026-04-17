e93d3dbc978fd4f6c9c9083b0f32c4bc8d66e6cd
kamartsbr <goldpentakill@gmail.com>
2026-04-17 17:29:35 -0300

    Atualizar o vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite'; // <--- A peça que faltava
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 
@@ -6,7 +7,10 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   
   return {
-    plugins: [react()],
+    plugins: [
+      react(),
+      tailwindcss(), // <--- Ativa o processamento do seu index.css
+    ],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },

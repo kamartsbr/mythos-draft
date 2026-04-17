40dea2a1b7de57b0fbbb96218e2309f0fdcee7af
kamartsbr <goldpentakill@gmail.com>
2026-04-17 16:57:31 -0300

    Atualizar o vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 
@@ -6,8 +7,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   
   return {
-    // Removemos o tailwindcss() daqui, pois na v3 ele roda pelo PostCSS
-    plugins: [react()],
+    plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
@@ -15,9 +15,6 @@ export default defineConfig(({ mode }) => {
       alias: {
         '@': path.resolve(__dirname, '.'),
       },
-    },
-    server: {
-      hmr: process.env.DISABLE_HMR !== 'true',
     },
     build: {
       outDir: 'dist',

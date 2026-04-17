3e7cd002289f9f5989f5846357a1c522dad447c4
kamartsbr <goldpentakill@gmail.com>
2026-04-17 18:02:44 -0300

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,8 +2,8 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
-// O segredo está aqui: import.meta.env
 const firebaseConfig = {
+  // Use EXATAMENTE essa linha abaixo, sem aspas ao redor do import
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY, 
   authDomain: "boxwood-plating-368522.firebaseapp.com",
   projectId: "boxwood-plating-368522",
